d9fdf68c8076eed3ad31ecb2c18eeab91dc8551e
Nicola VIGANÒ <nicola.vigano@cea.fr>
2026-01-09 10:31:54 +0100

    Add tracking of computation performance metrics
    Signed-off-by: Nicola VIGANÒ <nicola.vigano@cea.fr>

--- src/noise2ghost/reconstructions.py
+++ src/noise2ghost/reconstructions.py
@@ -5,14 +5,17 @@ This module provides functions for reconstructing images from noisy data using v
 from collections.abc import Callable, Sequence
 from concurrent.futures import Executor
 from copy import deepcopy
-from dataclasses import dataclass, field, replace
+from dataclasses import dataclass, field
 from pathlib import Path
+from time import perf_counter
 
 import corrct as cct
+from corrct.solvers import SolutionInfo
 import numpy as np
 from autoden.losses import LossRegularizer
 from autoden.models.io import load_model
 from autoden.models.config import NetworkParams, create_network
+from corrct.param_tuning import PerfMeterTask, PerfMeterBatch
 from corrct.struct_illum import MaskCollection
 from numpy.typing import NDArray
 from torch.nn import Module
@@ -68,7 +71,7 @@ def reconstruct_variational(
     iterations: int = 2000,
     reg: cct.regularizers.BaseRegularizer | None = None,
     verbose: bool = False,
-) -> tuple[NDArray, cct.solvers.SolutionInfo | None]:
+) -> tuple[NDArray, cct.solvers.SolutionInfo | None, PerfMeterTask]:
     """
     Perform variational reconstruction.
 
@@ -87,13 +90,17 @@ def reconstruct_variational(
 
     Returns
     -------
-    tuple[NDArray, cct.solvers.SolutionInfo | None]
+    tuple[NDArray, cct.solvers.SolutionInfo | None, PerfMeterTask]
         The reconstructed image and solver information.
     """
+    c0 = perf_counter()
+
     if not isinstance(masks, MaskCollection):
         masks = MaskCollection(masks)
 
     p = cct.struct_illum.ProjectorGhostImaging(masks)
+
+    c1 = perf_counter()
 
     if reg is None:
         rec = p.fbp(buckets, adjust_scaling=False)
@@ -104,7 +111,9 @@ def reconstruct_variational(
 
     rec = post_process_scale_bias(rec, masks, buckets)
 
-    return rec, info
+    c2 = perf_counter()
+
+    return rec, info, PerfMeterTask(init_time_s=c1 - c0, exec_time_s=c2 - c1, total_time_s=c2 - c0)
 
 
 def reconstruct_neural_cnn(
@@ -112,7 +121,7 @@ def reconstruct_neural_cnn(
     buckets: NDArray,
     rec_pars: RecParsCNN = RecParsCNN(),
     reg_val: float | LossRegularizer | None = None,
-) -> tuple[NDArray, dict[str, NDArray]]:
+) -> tuple[NDArray, dict[str, NDArray], PerfMeterTask]:
     """
     Perform neural network-based reconstruction using CNN.
 
@@ -130,8 +139,10 @@ def reconstruct_neural_cnn(
     Returns
     -------
     tuple
-        The reconstructed image and training losses.
+        The reconstructed image, training losses, and performance metrics.
     """
+    c0 = perf_counter()
+
     is_n2g = rec_pars.num_splits is not None
 
     model = _get_model(rec_pars.model)
@@ -145,6 +156,9 @@ def reconstruct_neural_cnn(
         tst_fraction=0.0,
         cv_fraction=rec_pars.cv_fraction,
     )
+
+    c1 = perf_counter()
+
     losses = solver_n2g.train(
         inp_recs_trn,
         tgt_trn_data,
@@ -162,7 +176,9 @@ def reconstruct_neural_cnn(
 
     gi_rec = post_process_scale_bias(gi_rec, masks, buckets)
 
-    return gi_rec, losses
+    c2 = perf_counter()
+
+    return gi_rec, losses, PerfMeterTask(init_time_s=c1 - c0, exec_time_s=c2 - c1, total_time_s=c2 - c0)
 
 
 def fit_variational_reg_weight(
@@ -174,7 +190,7 @@ def fit_variational_reg_weight(
     lower_limit: float | None = None,
     num_averages: int = 3,
     parallel_eval: bool | int | Executor = False,
-) -> tuple[float, NDArray]:
+) -> tuple[float, NDArray, PerfMeterBatch]:
     """
     Fit the regularization weight for variational reconstruction.
 
@@ -204,30 +220,33 @@ def fit_variational_reg_weight(
     """
     solver_verbose = not isinstance(parallel_eval, Executor)
 
-    def solver_spawn(lam: float):
-        return cct.solvers.PDHG(regularizer=reg(lam), verbose=solver_verbose, leave_progress=False)
-
-    def solver_call(solver: cct.solvers.Solver, mask: NDArray | None = None):
+    def solve_reg(lam: float, mask: NDArray | None = None) -> tuple[NDArray, SolutionInfo]:
+        solver = cct.solvers.PDHG(regularizer=reg(lam), verbose=solver_verbose, leave_progress=False)
         prj = cct.struct_illum.ProjectorGhostImaging(masks)
         return solver(prj, buckets, iterations=iterations, lower_limit=lower_limit, b_test_mask=mask)
 
     cv = cct.param_tuning.CrossValidation(
         buckets.shape, num_averages=num_averages, verbose=True, plot_result=True, parallel_eval=parallel_eval
     )
-    cv.solver_spawning_function = solver_spawn
-    cv.solver_calling_function = solver_call
+    cv.task_exec_function = solve_reg
 
-    lams = cv.get_lambda_range(*lambda_range, num_per_order=2)
-    f_avgs, _, _ = cv.compute_loss_values(lams)
+    lams = cct.param_tuning.get_lambda_range(*lambda_range, num_per_order=2)
+    f_avgs, _, all_info = cv.compute_loss_values(lams, return_all=True)
     lam_min, _ = cv.fit_loss_min(lams, f_avgs)
     print(lam_min)
 
-    solver = solver_spawn(lam_min)
-    rec_reg, _ = solver_call(solver)
+    c0 = perf_counter()
 
+    rec_reg, _ = solve_reg(lam_min)
     rec_reg = post_process_scale_bias(rec_reg, masks, buckets)
 
-    return lam_min, rec_reg
+    c1 = perf_counter()
+    final_rec_perfs = PerfMeterTask(init_time_s=0.0, exec_time_s=c1 - c0, total_time_s=c1 - c0)
+
+    stats = sum([info[2] for info in all_info], PerfMeterBatch())
+    stats.append(final_rec_perfs)
+
+    return lam_min, rec_reg, stats
 
 
 @dataclass
@@ -235,8 +254,8 @@ class TempResults:
     """Stores reconstruction results including input and output reconstructions and associated losses."""
 
     recs_in: NDArray
-    recs_out: list[NDArray] = field(default_factory=lambda: list())
-    losses: list[dict[str, NDArray]] = field(default_factory=lambda: list())
+    recs_out: list[NDArray] = field(default_factory=lambda: [])
+    losses: list[dict[str, NDArray]] = field(default_factory=lambda: [])
 
 
 def fit_neural_cnn_reg_weight(
@@ -245,7 +264,7 @@ def fit_neural_cnn_reg_weight(
     rec_pars: RecParsCNN,
     reg_vals: Sequence[float | None] | NDArray | float | None = None,
     device: str = "cuda",
-) -> tuple[float, NDArray, dict[str, NDArray]]:
+) -> tuple[float, NDArray, dict[str, NDArray], PerfMeterBatch]:
     """
     Fit the regularization weight for neural network-based reconstruction using CNN.
 
@@ -264,9 +283,11 @@ def fit_neural_cnn_reg_weight(
 
     Returns
     -------
-    tuple[float, NDArray, dict[str, NDArray]]
-        The best regularization weight, the reconstructed image, and training losses.
+    tuple[float, NDArray, dict[str, NDArray], PerfMeterBatch]
+        The best regularization weight, the reconstructed image, the training losses, and the performance metrics.
     """
+    cb0 = perf_counter()
+
     is_n2g = rec_pars.num_splits is not None
 
     model = _get_model(rec_pars.model)
@@ -283,10 +304,15 @@ def fit_neural_cnn_reg_weight(
 
     reg_vals = np.array(reg_vals, ndmin=1)
     results = TempResults(recs_in=recs_trn_inp)
+    stats_tasks = []
+
+    cb1 = perf_counter()
 
     for ii_r, reg_val in enumerate(reg_vals):
         print(f"{ii_r+1}/{len(reg_vals)} Lambda: {reg_val:.3e}")
+        ct0 = perf_counter()
         solver_n2g = N2G(model=deepcopy(model), reg_val=reg_val, data_scale_bias=data_sb, device=device)
+        ct1 = perf_counter()
         losses = solver_n2g.train(
             recs_trn_inp,
             data_trn_tgt,
@@ -303,9 +329,11 @@ def fit_neural_cnn_reg_weight(
             gi_rec = gi_rec.mean(axis=0)
 
         gi_rec = post_process_scale_bias(gi_rec, masks, buckets)
+        ct2 = perf_counter()
 
         results.recs_out.append(gi_rec)
         results.losses.append(losses)
+        stats_tasks.append(PerfMeterTask(init_time_s=(ct1 - ct0), exec_time_s=(ct2 - ct1), total_time_s=(ct2 - ct0)))
 
     min_losses = [np.nanmin(losses["loss_tst"]) for losses in results.losses]
     best_rec_ind = np.argmin(min_losses)
@@ -313,7 +341,9 @@ def fit_neural_cnn_reg_weight(
     cv = cct.param_tuning.CrossValidation(buckets.shape, verbose=True, plot_result=True)
     min_reg_weight, _ = cv.fit_loss_min(reg_vals, np.array(min_losses))
 
+    ct0 = perf_counter()
     solver_n2g = N2G(model=deepcopy(model), reg_val=min_reg_weight, data_scale_bias=data_sb, device=device)
+    ct1 = perf_counter()
     losses = solver_n2g.train(
         recs_trn_inp,
         data_trn_tgt,
@@ -329,8 +359,14 @@ def fit_neural_cnn_reg_weight(
         gi_rec = gi_rec.mean(axis=0)
 
     gi_rec = post_process_scale_bias(gi_rec, masks, buckets)
+    ct2 = perf_counter()
+
+    stats_tasks.append(PerfMeterTask(init_time_s=(ct1 - ct0), exec_time_s=(ct2 - ct1), total_time_s=(ct2 - ct0)))
+    cb2 = perf_counter()
+    stats_batch = PerfMeterBatch(init_time_s=cb1 - cb0, proc_time_s=cb2 - cb1, total_time_s=cb2 - cb0)
+
     print(f"{'N2G' if is_n2g else 'GIDC'}: Found lowest loss for lambda = {min_reg_weight} (ind: {best_rec_ind})")
-    return min_reg_weight, gi_rec, losses
+    return min_reg_weight, gi_rec, losses, stats_batch
 
 
 def fit_neural_inr_reg_weight(
@@ -340,7 +376,7 @@ def fit_neural_inr_reg_weight(
     epochs: int = 1024 * 6,
     device: str = "cuda",
     lower_limit: float | None = None,
-) -> tuple[float, NDArray, dict[str, NDArray]]:
+) -> tuple[float, NDArray, dict[str, NDArray], PerfMeterBatch]:
     """
     Fit the regularization weight for neural network-based reconstruction using INR.
 
@@ -359,9 +395,11 @@ def fit_neural_inr_reg_weight(
 
     Returns
     -------
-    tuple[float, NDArray, dict[str, NDArray]]
-        The best regularization weight, the reconstructed image, and training losses.
+    tuple[float, NDArray, dict[str, NDArray], PerfMeterBatch]
+        The best regularization weight, the reconstructed image, the training losses, and the performance metrics.
     """
+    cb0 = perf_counter()
+
     if reg_vals is None or isinstance(reg_vals, float):
         reg_vals = [reg_vals]
     model_def = NetworkParamsINR(n_features=512, n_layers=2, n_embeddings=256)
@@ -374,12 +412,17 @@ def fit_neural_inr_reg_weight(
 
     reg_vals = np.array(reg_vals, ndmin=1)
     results = TempResults(recs_in=encode_grid.detach().cpu().numpy().copy())
+    stats_tasks = []
+
+    cb1 = perf_counter()
 
     for ii_r, reg_val in enumerate(reg_vals):
         print(f"{ii_r+1}/{len(reg_vals)} Lambda: {reg_val:.3e}")
+        ct0 = perf_counter()
         solver_inr = INR(
             model=deepcopy(model), reg_val=reg_val, data_scaling_bias=data_sb, encoder=solver_inr_base.encoder, device=device
         )
+        ct1 = perf_counter()
         losses = solver_inr.train(
             encode_grid,
             data_trn_tgt,
@@ -393,9 +436,11 @@ def fit_neural_inr_reg_weight(
         gi_rec = solver_inr.infer(encode_grid).mean(axis=0)
 
         gi_rec = post_process_scale_bias(gi_rec, masks, buckets)
+        ct2 = perf_counter()
 
         results.recs_out.append(gi_rec)
         results.losses.append(losses)
+        stats_tasks.append(PerfMeterTask(init_time_s=(ct1 - ct0), exec_time_s=(ct2 - ct1), total_time_s=(ct2 - ct0)))
 
     min_losses = [np.nanmin(losses["loss_tst"]) for losses in results.losses]
     best_rec_ind = np.argmin(min_losses)
@@ -403,6 +448,7 @@ def fit_neural_inr_reg_weight(
     cv = cct.param_tuning.CrossValidation(buckets.shape, verbose=True, plot_result=True)
     min_reg_weight, _ = cv.fit_loss_min(reg_vals, np.array(min_losses))
 
+    ct0 = perf_counter()
     solver_inr = INR(
         model=deepcopy(model),
         reg_val=min_reg_weight,
@@ -410,11 +456,18 @@ def fit_neural_inr_reg_weight(
         encoder=solver_inr_base.encoder,
         device=device,
     )
+    ct1 = perf_counter()
     losses = solver_inr.train(
         encode_grid, data_trn_tgt, data_val_tgt, epochs=epochs, algo="adam", learning_rate=1e-4, weight_decay=0.0
     )
     gi_rec = solver_inr.infer(encode_grid).mean(axis=0)
 
     gi_rec = post_process_scale_bias(gi_rec, masks, buckets)
+    ct2 = perf_counter()
+
+    stats_tasks.append(PerfMeterTask(init_time_s=(ct1 - ct0), exec_time_s=(ct2 - ct1), total_time_s=(ct2 - ct0)))
+    cb2 = perf_counter()
+    stats_batch = PerfMeterBatch(init_time_s=cb1 - cb0, proc_time_s=cb2 - cb1, total_time_s=cb2 - cb0)
+
     print(f"INR: Found lowest loss for lambda = {min_reg_weight} (ind: {best_rec_ind})")
-    return min_reg_weight, gi_rec, losses
+    return min_reg_weight, gi_rec, losses, stats_batch
